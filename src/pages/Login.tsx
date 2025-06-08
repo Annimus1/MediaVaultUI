4b81c8a12e2d7245c13d661211e2310c75017e9a
Pablo Vergara <pablo.j98@gmail.com>
2025-06-08 12:29:41 -0400

    Fix: Login
    - Fix display bug in Mobile

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -34,11 +34,10 @@ function Login() {
   return (
     <div className='w-full h-[100vh] flex items-center justify-center  bg-center 
     bg-[url("https://images.unsplash.com/photo-1629180774299-b0d0adc288e7?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")]'>
-
+      <div className='w-full h-dvh bg-black/10 fixed'></div>
       <div className={error.status ? "tooltip tooltip-open tooltip-error" : ""} data-tip={error.message}>
-        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-md h-[22rem] border p-4 m-4 ">
+        <fieldset className="w-xs fieldset bg-base-200 border-base-300 rounded-box border p-4 md:w-lg lg:w-xl xl:w-[38rem]">
           <legend className="fieldset-legend text-2xl">Login</legend>
-
 
           <label className="label select-none text-lg" htmlFor='user'>Email | Username</label>
           <input type="text" id='user' onChange={(text) => setUser(text.target.value)} value={user} className="input w-full text-lg focus:outline-0" placeholder="Email | username" />
@@ -50,7 +49,7 @@ function Login() {
 
           <br />
 
-          <button disabled={!(user.length>0) || !(password.length>0)} className="btn btn-neutral mt-4 text-lg" onClick={() => { login() }}>Login {loading ? <span className="loading loading-dots loading-md"></span> : ""}</button>
+          <button disabled={!(user.length>0) || !(password.length>0)} className="btn btn-neutral mt-4 text-lg z-10" onClick={() => { login() }}>Login {loading ? <span className="loading loading-dots loading-md"></span> : ""}</button>
 
           <p>Don't have an account yet? <Link to="/register" className="link link-accent">Register</Link> here.</p>
         </fieldset>
